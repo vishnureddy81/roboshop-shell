531dac2b885c0928199b82cd484c350f52b8cb16
vishnureddy <tarigopula34@gmail.com>
2025-07-14 16:29:30 +0530

    mongodb

---
+++ catalogue.sh
@@ -0,0 +1,25 @@
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
+cp mongodb /etc/systemd/system/catalogue.service
+
+
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+
+useradd roboshop
+
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+cd /app
+unzip /tmp/catalogue.zip
+
+cd /app
+npm install
+
+dnf install mongodb-mongosh -y
+mongosh --host mongodb.vishnuredddy.online </app/db/master-data.js
+
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl restart catalogue
+
