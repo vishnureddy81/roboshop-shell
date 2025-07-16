0242090ae5581fdc02f56d456960a846fee1c72d
vishnureddy <tarigopula34@gmail.com>
2025-07-16 09:53:03 +0530

    The script has been executed.

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,6 @@
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 dnf install mongodb-org -y
 
-sed -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod
 systemctl restart mongod
